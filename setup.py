3d9c1bd54f9e1303a15ef3f171451b3b6742ae49
A. Stamminger <andreas.stamminger@pm.me>
2019-10-21 18:24:22 +0200

    Initialized project

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="aiida-project",
+    version="0.0.0",
+    author="Andreas Stamminger",
+    author_email="stammingera@gmail.com",
+    description="Utility for setting up and managing AiiDA projects",
+    long_description="",
+    keywords="",
+    url="", 
+    classifiers=[
+        ''
+    ],
+)
