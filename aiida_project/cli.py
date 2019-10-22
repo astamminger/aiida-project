086d866e7e4ab76cce3b1b77ee476e83d861617b
A. Stamminger <andreas.stamminger@pm.me>
2019-10-22 21:45:05 +0200

    Add options to cli.create command

--- aiida_project/cli.py
+++ aiida_project/cli.py
@@ -8,7 +8,18 @@ def main():
     pass
 
 
-@main.command('create')
-def create():
-    """Create a new AiiDA project environment."""
+@main.command()
+@click.argument('name', type=str)
+@click.option('--manager', type=click.Choice(["conda"]),
+              help="Manager to be used for creating the environment")
+@click.option('--aiida-version', 'version', type=str,
+              help=("AiiDA version to install. This may be either a version "
+                    "string if conda is used but also a github url for "
+                    "virtualenv or virtualenvwrapper"))
+@click.option('--python', type=str, help="The environment's python version")
+@click.option('--plugins', multiple=True, type=str,
+              help=("One or multiple plugins which will be installed to the "
+                    "environment"))
+def create(name, manager, version, python, plugins):
+    """Create a new AiiDA project environment with name NAME."""
     pass
---
+++ aiida_project/create.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+Create AiiDA project environment
+"""
