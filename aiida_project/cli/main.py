a3bd5613a5ffeda9228cc93f4410db3e91282d8f
A. Stamminger <andreas.stamminger@pm.me>
2019-10-22 18:35:26 +0200

    Add create subcommand to main CLI

--- aiida_project/cli/main.py
+++ aiida_project/cli/main.py
@@ -4,4 +4,11 @@ import click
 
 @click.group('aiida-project')
 def main():
+    """Create and manage AiiDA projects."""
+    pass
+
+
+@main.command('create')
+def create():
+    """Create a new AiiDA project environment."""
     pass
