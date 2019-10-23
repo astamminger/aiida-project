3922d82b7961dff6211945d886a50664c3e07140
A. Stamminger <andreas.stamminger@pm.me>
2019-10-23 20:25:40 +0200

    Add utility file

---
+++ aiida_project/utils.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+def clone_git_repo_to_disk(github_url, location):
+    """
+    Clone the git repository at github_url to location on disk.
+
+    :param str github_url: URL to github repository
+    :param str location: path to the location disk
+    """
+    pass
