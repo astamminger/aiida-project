c9ac495a94a1d32838757b3fa740097634c3ed17
A. Stamminger <andreas.stamminger@pm.me>
2019-10-24 18:41:14 +0200

    Update argument list for create command

--- aiida_project/cli.py
+++ aiida_project/cli.py
@@ -10,16 +10,26 @@ def main():
 
 @main.command()
 @click.argument('name', type=str)
-@click.option('--manager', type=click.Choice(["conda"]),
+@click.option('--manager', type=click.Choice(["conda", "venv"]),
+              default="venv",
               help="Manager to be used for creating the environment")
-@click.option('--aiida-version', 'version', type=str,
+@click.option('--aiida', 'aiida_version', type=str, default=None,
               help=("AiiDA version to install. This may be either a version "
                     "string if conda is used but also a github url for "
                     "virtualenv or virtualenvwrapper"))
-@click.option('--python', type=str, help="The environment's python version")
+@click.option('--python', 'python_version', type=str, default="3.6",
+              help="The environment's python version")
 @click.option('--plugin', 'plugins', multiple=True, type=str,
               help=("One or multiple plugins which will be installed to the "
                     "environment"))
-def create(name, manager, version, python, plugins):
+@click.option('--pkg', 'pkgs', multiple=True, type=str,
+              help=("One or multiple additional packages to be installed to "
+                    "the environment."))
+def create(name, manager, aiida_version, python_version, plugins, pkgs):
     """Create a new AiiDA project environment with name NAME."""
+    print(manager)
+    print(aiida_version)
+    print(python_version)
+    print(plugins)
+    print(pkgs)
     pass
