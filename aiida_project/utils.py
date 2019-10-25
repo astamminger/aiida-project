3b7380094d62fd9e980527e198cc184b33995eff
A. Stamminger <andreas.stamminger@pm.me>
2019-10-25 15:17:00 +0200

    Implement function to clone from GitHUB repositories

--- aiida_project/utils.py
+++ aiida_project/utils.py
@@ -1,16 +1,31 @@
 # -*- coding: utf-8 -*-
 
 import re
+import subprocess
+
+import click_spinner
 
 
-def clone_git_repo_to_disk(github_url, location):
+def clone_git_repo_to_disk(github_url, branch, location):
     """
     Clone the git repository at github_url to location on disk.
 
     :param str github_url: URL to github repository
+    :param str branch: Specific branch of the github repository
     :param str location: path to the location disk
     """
-    pass
+    git_clone_command = ("git clone --single-branch --branch {branch} "
+                         " {url} {path}".format(branch=branch, url=github_url,
+                                                path=location))
+    proc = subprocess.Popen(git_clone_command, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, shell=True)
+    print("Cloning repository {} ...".format(github_url))
+    with click_spinner.spinner():
+        stdout, stderr = proc.communicate()
+    if proc.returncode:
+        raise Exception("Cloning the repository from GitHub failed. Used "
+                        "command {}, STDERR={}"
+                        .format(git_clone_command, stderr.decode()))
 
 
 def assert_valid_aiida_version(aiida_version_string):
@@ -52,3 +67,26 @@ def disassemble_package_def(package_definition):
     :rtype: tuple
     """
     return (re.split(r"[\/\:]", package_definition) + [None])[:3]
+
+
+def check_command_avail(command, test_version=True):
+    """
+    Test if a command is available in the current shell environment.
+
+    :param str command: Command to test
+    :param bool test_version: If `True` command --version will be checked
+        instead of the plain command
+    """
+    # run command --version because some commands do not exit with
+    # exitcode 0 when called without any arguments (i.e. git)
+    if test_version:
+        command_to_check = "{} --version".format(command)
+    else:
+        command_to_check = command
+    proc = subprocess.Popen(command_to_check, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, shell=True)
+    stdout, stderr = proc.communicate()
+    if proc.returncode:
+        return False
+    else:
+        return True
