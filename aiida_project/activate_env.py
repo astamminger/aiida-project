b8367ca94f43a0c815355055e6c27300fff7bd7e
A. Stamminger <andreas.stamminger@pm.me>
2019-10-31 19:30:45 +0100

    Start activator base class used for activation

---
+++ aiida_project/activate_env.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+Activate AiiDA projects and export required variables
+"""
+
+
+class ActivateEnvBase(object):
+    """Base class for activating AiiDA projects."""
+
+    # export_cmd
+    # ...
