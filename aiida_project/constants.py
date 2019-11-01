8174dddbc7d337155d484ce188392af15fc8bea6
A. Stamminger <andreas.stamminger@pm.me>
2019-11-01 20:11:25 +0100

    Add constants file to store global variables

---
+++ aiida_project/constants.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+
+"""
+Constants used throughout the project
+"""
+
+
+ON_WIN = bool(sys.platform == "win32")
+
+# default project subdirectories
+DEFAULT_SRC_SUBFOLDER = "src"
+DEFAULT_ENV_SUBFOLDER = "env"
+AIIDA_SUBFOLDER = ".aiida"
+
+# configuration file
+CONFIG_FOLDER = ".aiida_project"
+PROJECTS_FILE = ".projects"
+
+# compatible shells
+if ON_WIN:
+    COMPATIBLE_SHELLS = [
+        'bash',
+    ]
+else:
+    COMPATIBLE_SHELLS = [
+        'bash',
+    ]
