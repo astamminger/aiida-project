413d77783379e2951da744d8a468451f0d31e3c6
A. Stamminger <andreas.stamminger@pm.me>
2019-11-05 21:40:39 +0100

    use actually available paths in pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -94,22 +94,23 @@ def temporary_home(monkeypatch, temporary_folder):
 
 @pytest.fixture
 def project_spec_file(temporary_home):
+    home = pathlib.Path.home()
     venv_env = {
         'project_name': 'virtualenv_project',
-        'project_path': '/path/to/project/virtualenv_project',
+        'project_path': str(home / 'virtualenv_project'),
         'aiida': '1.0.0',
         'python': '3.6',
-        'env_sub': '/path/to/project/virtualenv_project/env',
-        'src_sub': '/path/to/project/virtualenv_project/src',
+        'env_sub': str(home / 'virtualenv_project' / 'env'),
+        'src_sub': str(home / 'virtualenv_project' / 'src'),
         'manager': constants.MANAGER_NAME_VENV,
     }
     conda_env = {
         'project_name': 'conda_project',
-        'project_path': '/path/to/project/conda_project',
+        'project_path': str(home / 'conda_project'),
         'aiida': '1.0.0',
         'python': '3.6',
-        'env_sub': '/path/to/project/conda_project/env',
-        'src_sub': '/path/to/project/conda_project/src',
+        'env_sub': str(home / 'conda_project' / 'env'),
+        'src_sub': str(home / 'conda_project' / 'src'),
         'manager': constants.MANAGER_NAME_CONDA,
     }
     utils.save_project_spec(venv_env)
