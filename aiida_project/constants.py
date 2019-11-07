ad9ff05b95652a13aa3116113808a72345bd8382
A. Stamminger <andreas.stamminger@pm.me>
2019-11-07 18:17:41 +0100

    add internal shell names to constants.py

--- aiida_project/constants.py
+++ aiida_project/constants.py
@@ -20,16 +20,19 @@ AIIDA_SUBFOLDER = ".aiida"
 CONFIG_FOLDER = ".aiida_project"
 PROJECTS_FILE = ".projects.yaml"
 
-# compatible shells
-if ON_WIN:
-    COMPATIBLE_SHELLS = [
-        'bash',
-    ]
-else:
-    COMPATIBLE_SHELLS = [
-        'bash',
-    ]
-
 # define internal names for package managers
 MANAGER_NAME_CONDA = 'conda'
 MANAGER_NAME_VENV = 'virtualenv'
+
+# define internal names for shells
+SHELL_NAME_BASH = 'bash'
+
+# compatible shells
+if ON_WIN:
+    COMPATIBLE_SHELLS = [
+        SHELL_NAME_BASH,
+    ]
+else:
+    COMPATIBLE_SHELLS = [
+        SHELL_NAME_BASH,
+    ]
